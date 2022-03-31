18e5cb4900e9a855c88dd53fa4341f3fd24d1f21
pk-3 <pk-3@sr.dmcs.pl>
2022-03-31 21:09:25 +0200

    Initial commit

---
+++ src/main/java/com/example/plantsforyou/PlantsForYouApplication.java
@@ -0,0 +1,13 @@
+package com.example.plantsforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantsForYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantsForYouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/plantsforyou/PlantsForYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plantsforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantsForYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
